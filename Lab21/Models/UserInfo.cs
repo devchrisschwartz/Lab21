8b7f0555623c856b8ff3a6671e2ee963769d66e9
chris <tarancello@gmail.com>
2018-08-15 16:55:57 -0400

    Finished client and server validation

--- Lab21/Models/UserInfo.cs
+++ Lab21/Models/UserInfo.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab21.Models
 {
     public class UserInfo
     {
+        [Required]
+        [RegularExpression("[a-zA-Z]{2,30}")]
         public string FirstName { set; get; }
+
+        [Required]
+        [RegularExpression("[a-zA-Z]{2,30}")]
         public string LastName { set; get; }
+
+        [Required]
+        [RegularExpression(@"\w{2,30}@[a-zA-Z]{2,15}\.[a-zA-Z]{3}")]
         public string Email { set; get; }
+
+        [RegularExpression(@"\d{10}|\d{3}-\d{3}-\d{4}")]
         public string Num { set; get; }
+
+        [Required]
+        [RegularExpression("[a-zA-Z]{2,30}")]
         public string Pass { set; get; }
 
         public UserInfo()
