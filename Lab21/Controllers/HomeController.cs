dd4d19cf59375407312a9ccf1890a0734b7c3aec
chris <tarancello@gmail.com>
2018-08-15 16:22:26 -0400

    initial commit

--- Lab21/Controllers/HomeController.cs
+++ Lab21/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Lab21.Models;
 
 namespace Lab21.Controllers
 {
@@ -23,6 +24,18 @@ namespace Lab21.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult AddUser(UserInfo newUser)
+        {
+            ViewBag.Message = $"Welcome, {newUser.FirstName}";
 
             return View();
         }
---
+++ Lab21/Models/UserInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Models
+{
+    public class UserInfo
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Email { set; get; }
+        public string Num { set; get; }
+        public string Pass { set; get; }
+
+        public UserInfo()
+        {
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            Num = "";
+            Pass = "";
+        }
+
+        public UserInfo(string fn, string ln, string e, string t, string pass)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Email = e;
+            Num = t;
+            Pass = pass;
+        }
+    }
+}
